4d58b0c015bbda3d647e927457f88ef8a781688b
Roshan <88413854+Twinn-github09@users.noreply.github.com>
2024-06-19 18:44:43 +0530

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "G:\\flutter_windows_3.19.6-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\Code\\AIOT\\DogCat\\my_flutter_app" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=G:\\flutter_windows_3.19.6-stable\\flutter"
+  "PROJECT_DIR=D:\\Code\\AIOT\\DogCat\\my_flutter_app"
+  "FLUTTER_ROOT=G:\\flutter_windows_3.19.6-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\Code\\AIOT\\DogCat\\my_flutter_app\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\Code\\AIOT\\DogCat\\my_flutter_app"
+  "FLUTTER_TARGET=D:\\Code\\AIOT\\DogCat\\my_flutter_app\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9jNGNkNDhlMTg2NDYwYjMyZDQ0NTg1Y2UzYzEwMzI3MWFiNjc2MzU1Lw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\Code\\AIOT\\DogCat\\my_flutter_app\\.dart_tool\\package_config.json"
+)
